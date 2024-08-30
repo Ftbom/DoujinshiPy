c5c019d29bab3bd0b09c2eaf9f891af18b51561a
Ftbom <lz490070@gmail.com>
2024-08-30 13:26:12 +0800

    更新 install.py

--- install.py
+++ install.py
@@ -1,17 +1,19 @@
 import os
 import json
 
+print("通过pip安装python包")
+os.system("pip install -r requirements.txt")
 # create data folder
-print("create data folder...")
+print("创建数据文件夹...")
 os.makedirs(".data", exist_ok = True)
 # create doujinshi library folder
 os.makedirs(".data/doujinshi", exist_ok = True)
 # create config file
 if os.path.exists(".data/config.json"):
-    c = input("config file already exist, replace or not? [Y/n]")
+    c = input("配置文件已存在，是否替换？[Y/n]")
     if not c in ["", "y", "Y"]:
         exit()
-print("create config file...")
+print("创建配置文件...")
 with open(".data/config.json", "w", encoding = "utf-8") as f:
     f.write(json.dumps({
         "settings": {
@@ -31,5 +33,5 @@ with open(".data/config.json", "w", encoding = "utf-8") as f:
         }
     }, indent = 4))
 print("--------------------------------")
-print("config file: .data/config.json")
-print("start application: python app.py")
+print("配置文件：.data/config.json")
+print("启动：python app.py")
