a74eb004403e3cef114131ebbc4b3c8c709c93d4
ftbom <lz490070@gmail.com>
2025-04-29 18:39:01 +0800

    update pwa

--- static/sw.js
+++ static/sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = "pwa-cache-v1";
-const urlsToCache = ["/web/page", "/web/batch", "/web/group", "/web/other",
+const urlsToCache = ["/web/page", "/web/tags", "/web/batch", "/web/group", "/web/other",
     "/web/static/styles.css", "/web/static/doujinshi.js", "/web/static/ehtag.js",
     "/web/static/icons/favicon.ico", "/web/static/icons/icon-192x192.png", "/web/static/icons/icon-512x512.png"];
 
